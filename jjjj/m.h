3c9a53f8b8d1e750dd8876e5f530c723315edeca
Amitav Chris Mostafa <amitavcmostafa@gmx.de>
2023-02-27 22:37:42 +0100

    Commit

---
+++ jjjj/m.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    // "printf()" belongs to stdio.h
+    printf("hello world");
+    return 0;
+}
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from PyPDF2 import PdfFileReader
 from pathlib import Path
+import shutil 
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
 #Create new instance of PDfFileReader class
 #First get Padth to PDF File
 
-pdf_path = ( Path.home()/'HESSENBOX-DA'/'Austausch Amitav - Evaluation MDZ (Phillip Bausch)'/'Stundenschreibung'/'Offizielle Liste copy.pdf')
+pdf_path = ( Path.home()/'HESSENBOX-DA'/'Austausch Amitav - Evaluation MDZ (Phillip Bausch)'/'Stundenschreibung'/'Dummy.pdf')
 
-#It was showing erros because Path.home() brings me to my home direction which is already Users/acm/
+#It was showing errors because Path.home() brings me to my home direction which is already Users/acm/
 #Having it again in string meant having Users/acm/Users/acm instead of Users/acm
 #Hence, beachten wo deine Home Directory liegt
 
